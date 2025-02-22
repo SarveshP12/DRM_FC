e398be7d4bcad37de64bbeb511aeac0ea4a36a1c
Jagguyrie <jagguyrie@gmail.com>
2025-02-22 21:43:26 +0530

    blurry boi

--- eventflow/src/app/components/navbar.js
+++ eventflow/src/app/components/navbar.js
@@ -1,15 +1,11 @@
-"use client"; 
-import React, { useState, useEffect } from "react";
+"use client";
+import React from "react";
 import { Ticket, Calendar } from "lucide-react"; // Icons from Lucide-react
 import { UserButton, SignedIn, SignedOut, SignInButton, SignUpButton } from "@clerk/nextjs";
 
 const Navbar = () => {
   return (
-    <nav className="w-full p-4 flex justify-between items-center bg-transparent fixed z-10">
-      <h1 className="text-white text-2xl font-bold user-select: none">
-        Event<span className="text-[#6a2c62]">FLOW</span>
-    <nav className="w-full max-w-7xl mx-auto p-4 flex justify-between items-center bg-transparent fixed top-6 left-1/2 transform -translate-x-1/2 px-10 border-2 border-[#A0B4E5] rounded-full shadow-md">
-      
+    <nav className="w-full px-4 flex justify-between items-center bg-white/10 backdrop-filter bg-blur fixed shadow-md">
       {/* Logo */}
       <h1 className="text-[#e8b878] text-2xl font-bold">
         Event<span className="text-white">FLOW</span>
@@ -29,26 +25,26 @@ const Navbar = () => {
 
       {/* Authentication Section */}
       <div className="flex items-center gap-6">
-        {/* Create Event Button (Placed before Sign Up) */}
-        <button className="px-8 py-3 text-lg text-white bg-transparent hover:bg-[#4a1e4a] transition-all">
+        {/* Create Event Button */}
+        <button className="p-2 text-lg text-white bg-transparent border border-[#6a2c62] rounded-full hover:bg-[#d78a427f] transition-all">
           Create Event
         </button>
 
+        {/* Sign Up and Login Buttons */}
         <SignedOut>
           <SignUpButton mode="modal">
-            <button
-              className="bg-[#6a2c62] text-white px-4 py-2 rounded-md hover:bg-[#4a1e4a] transition-colors">
+            <button className="bg-[#6a2c62] text-white p-2 my-2 rounded-full hover:bg-[#d78a427f] transition-colors">
               Sign Up
             </button>
           </SignUpButton>
           <SignInButton mode="modal">
-            <button
-              className="bg-transparent border border-[#6a2c62] text-[#6a2c62] px-4 py-2 rounded-md hover:bg-[#6a2c62] hover:text-white transition-colors">
+            <button className="bg-transparent border border-[#6a2c62] text-white p-2 my-2 rounded-full hover:bg-[#d78a427f] hover:text-white transition-colors">
               Login
             </button>
           </SignInButton>
         </SignedOut>
 
+        {/* User Button */}
         <SignedIn>
           <UserButton afterSignOutUrl="/" />
         </SignedIn>
